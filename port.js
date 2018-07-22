856bbae1700d01d46ad88346f8cbee3a0e6dac3f
Travis Virgil <travis@travisvirgil.com>
2018-07-21 20:17:06 -0400

    Add js mobile menu toggle

---
+++ port.js
@@ -0,0 +1,7 @@
+function classToggle() {
+  const navs = document.querySelectorAll('.mobileToggle')
+
+  navs.forEach(nav => nav.classList.toggle('.mobileShow'));
+}
+document.querySelector('#toggle')
+  .addEventListener('click', classToggle);
