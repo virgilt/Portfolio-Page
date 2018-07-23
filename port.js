37ae0b44b767990551627896d3944479e5c9babb
Travis Virgil <travis@travisvirgil.com>
2018-07-22 20:44:43 -0400

    navbar mobile responsive

--- port.js
+++ port.js
@@ -1,7 +1,6 @@
 function classToggle() {
   const navs = document.querySelectorAll('.mobileToggle')
 
-  navs.forEach(nav => nav.classList.toggle('.mobileShow'));
+  navs.forEach(nav => nav.classList.toggle('mobileShow'));
 }
-document.querySelector('#toggle')
-  .addEventListener('click', classToggle);
+document.querySelector('#toggle').addEventListener('click', classToggle);
